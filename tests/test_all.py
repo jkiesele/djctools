dbe3c702ebf05b2ecad0b9e02dac9456af483884
Jan Kieseler <jkiesele@cern.ch>
2024-11-07 08:59:14 +0100

    main test

---
+++ tests/test_all.py
@@ -0,0 +1,7 @@
+import unittest
+from test_logging_module import TestLoggingModule
+from test_loss_module import TestLossModule
+from test_wandb_wrapper import TestWandbWrapper
+
+if __name__ == "__main__":
+    unittest.main()
