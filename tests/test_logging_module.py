57caf182514313c27ed411b0cd109f2adb69fa1c
Jan Kieseler <jkiesele@cern.ch>
2024-11-08 10:11:57 +0100

    logging module updates and tests

--- tests/test_logging_module.py
+++ tests/test_logging_module.py
@@ -1,7 +1,7 @@
 import unittest
-from djctools.module_extensions import LoggingModule
+from djctools.module_extensions import LoggingModule, switch_all_logging
 from djctools.wandb_tools import wandb_wrapper
-
+import torch
 
 class TestLoggingModule(unittest.TestCase):
 
@@ -48,6 +48,47 @@ class TestLoggingModule(unittest.TestCase):
         module.switch_logging(False)
         module.log("test_metric_off", 2)
         self.assertNotIn("LoggingModule1_test_metric_off", wandb_wrapper.log_buffer)
+
+    def test_switch_all_logging(self):
+        
+        """Test that switch_all_logging toggles logging for all modules."""
+        class DerivedModule(LoggingModule):
+            def compute_metrics(self, x, y):
+                x = torch.sum(x**2 + y**2)
+                self.log("test_metric", x)
+
+        class MultiLoggingModule(torch.nn.Module):
+            def __init__(self, logging_active=True):
+                super().__init__()
+                self.module1 = DerivedModule(logging_active=logging_active, name = "Module1")
+                self.module2 = DerivedModule(logging_active=logging_active, name = "Module2")
+
+            def forward(self, x, y=None):
+                self.module1(x,y)
+                self.module2(x,y)
+                return x
+        mmodule = MultiLoggingModule()
+
+        #run some input through it and check that the logs are recorded
+        mmodule(torch.tensor(3), torch.tensor(4))
+        self.assertIn("Module1_test_metric", wandb_wrapper.log_buffer)
+        self.assertIn("Module2_test_metric", wandb_wrapper.log_buffer)
+
+        #clear buffer
+        wandb_wrapper.log_buffer.clear()
+
+        # Now switch off all logging and check that new logs are not recorded
+        switch_all_logging(mmodule, False)
+        mmodule(torch.tensor(5), torch.tensor(6))
+        self.assertNotIn("Module1_test_metric", wandb_wrapper.log_buffer)
+        self.assertNotIn("Module2_test_metric", wandb_wrapper.log_buffer)
+
+        #now check that the model also works without giving the "y" argument
+        try:
+            mmodule(torch.tensor(7))
+        except:
+            self.fail("Error in running the model without the 'y' argument")
+        
 
     def test_nested_module_logging(self):
         """Test that nested modules respect the logging state of the parent."""
